822a2b59d8a2dc551e5a77b40d538fd05f46b840
banannath <hanna.nathania@gmail.com>
2024-05-29 10:53:59 +0700

    tambahin file .js untuk atur tabel list saham dan benerin dikit list-stock.html

---
+++ static/stocktable.js
@@ -0,0 +1,56 @@
+(function () {
+    'use strict'
+
+    feather.replace({ 'aria-hidden': 'true' })
+
+    fetch('/stockdata')      
+    .then(response => response.json())
+    .then(data => {
+        const data_indo = data[0];
+        const data_us = data[1];
+        if (data_indo.error || data_us.error) {
+            console.error(data_indo.error || data_us.error);
+            return;
+        }
+
+        console.log(data_indo);
+        console.log(data_us);
+
+        var tableIndo = document.getElementById('Indo');
+        var tableUs = document.getElementById('Us');
+
+        let tableContentIndo = '';
+        let tableContentUs = '';
+
+        data_indo.forEach((stock, index) => {
+            tableContentIndo += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${stock.emiten}</td>
+                <td>${stock.close}</td>
+                <td>${stock.volume}</td>
+            </tr>
+            `;
+        });
+
+        data_us.forEach((stock, index) => {
+            tableContentUs += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${stock.emiten}</td>
+                <td>${stock.close}</td>
+                <td>${stock.volume}</td>
+            </tr>
+            `;
+        });
+
+        tableIndo.innerHTML = tableContentIndo;
+        tableUs.innerHTML = tableContentUs;
+
+        document.getElementById('tableUS').style.display = 'table';
+        document.getElementById('tableID').style.display = 'table';
+    })
+    .catch(error => {
+        console.error('Error fetching stock data:', error);
+    });
+})();
